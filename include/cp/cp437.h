aab4e5cdcaec58ee8ba989ffa0a014af4c020461
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2022-12-17 23:17:44 -0500

    Fix amount of setup ways
    Should be three now.

--- include/cp/cp437.h
+++ include/cp/cp437.h
@@ -34,7 +34,7 @@
     https://github.com/Journeyman-dev/cp437.h
 
     HOW TO SETUP
-    The cp437.h library can be included in your project in one of two different ways:
+    The cp437.h library can be included in your project in one of three different ways:
         - Copy and paste the cp437.h file directly into your source tree.
         - Clone the GitHub as a git submodule to your project's repository.
           1. In bash console from the root directory of your project's repository:
