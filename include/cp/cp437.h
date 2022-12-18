c9d7440ea82beda25b7c3b08f66ec651d426b692
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2022-12-17 23:14:02 -0500

    Fixes to doc header comment
    Its unicode not ascii. Spell out codepage entirely.

--- include/cp/cp437.h
+++ include/cp/cp437.h
@@ -65,8 +65,8 @@
         #include <cp/cp437.h>
 
      Now, you can include this header in any other translation unit, and have access to the wchar_t
-     array CP_UNICODE_LOOKUP_TABLE, which contains the unicode codes of every cp437 glyph. This
-     header also includes macros for each codepage 437 glyph to quickly get the ascii code.
+     array CP_UNICODE_LOOKUP_TABLE, which contains the unicode codes of every codepage 437 glyph. This
+     header also includes macros for each codepage 437 glyph to quickly get the unicode wchar_t value.
    CHANGELOG
    - Version 1.2
        - Change "Pipe" character to "Broken Bar" to follow the IBM standard.
