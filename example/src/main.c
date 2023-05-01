7f65bf7c1696c521dd1b157f51064faebfb2c2bf
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2023-04-30 20:18:08 -0400

    Update example
    Replace the first character of the first row with a space to prevent null termination character preventing the row from being printed. Change the variable name of the lookup table according with changes to the library.

--- example/src/main.c
+++ example/src/main.c
@@ -16,14 +16,18 @@ int main()
     {
         wchar_t wstr[33];
         size_t start = glyph_y * 32;
-        memcpy(wstr, CP_UNICODE_LOOKUP_TABLE + start, 32 * sizeof(wchar_t));
+        memcpy(wstr, CP_WCHAR_LOOKUP_TABLE + start, 32 * sizeof(wchar_t));
         wstr[32] = L'\0';
+        if (glyph_y == 0)
+        {
+            wstr[0] = L' ';
+        }
         wprintf(L"%ls\n", wstr);
     }
     wprintf(L"\nGlyphs:\n");
     for (int glyph_i = 0; glyph_i < CP_CODEPAGE_SIZE; glyph_i++)
     {
-        wprintf(L"%3d %lc\n", glyph_i, CP_UNICODE_LOOKUP_TABLE[glyph_i]);
+        wprintf(L"%3d %lc\n", glyph_i, CP_WCHAR_LOOKUP_TABLE[glyph_i]);
     }
     return 0;
 }
